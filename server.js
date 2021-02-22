c2acfac9fb5e4e965f4aeb07a782051ef189e771
siteson <1366713146@qq.com>
2021-02-23 00:16:17 +0800

    important ui backup

--- server.js
+++ server.js
@@ -16,54 +16,35 @@ var server = app.listen(8080, function () {
 io = io.listen(server, { cors: true });
  
 
-//在线用户
-var onlineUsers = {};
-//当前在线人数
-var onlineCount = 0;
- 
-io.on('connection', function(socket){
-    console.log('a user connected');
-     
-    //监听新用户加入
-    socket.on('login', function(obj){
-        //将新加入用户的唯一标识当作socket的名称，后面退出的时候会用到
-        socket.name = obj.userid;
-         
-        //检查在线列表，如果不在里面就加入
-        if(!onlineUsers.hasOwnProperty(obj.userid)) {
-            onlineUsers[obj.userid] = obj.username;
-            //在线人数+1
-            onlineCount++;
+var onlineUsers = new Array();
+// 公共空间
+const pub_space = io.of('/');
+pub_space.on('connection', function(socket){
+    // 用户加入，自定义事件
+    socket.on('login',function(user){
+        if(!onlineUsers.hasOwnProperty(socket.id)) {
+            //在线用户定义
+            onlineUsers[socket.id] = {
+                username: user.username,
+                avatar: user.avatar
+            }
         }
-         
-        //向所有客户端广播用户加入
-        io.emit('login', {onlineUsers:onlineUsers, onlineCount:onlineCount, user:obj});
-        console.log(obj.username+'加入了聊天室');
+        pub_space.broadcast.to(socket.id).emit('attend', onlineUsers);
+        console.log('['+onlineUsers[socket.id]+']加入了公共大厅');
     });
-     
-    //监听用户退出
+    // 用户退出
     socket.on('disconnect', function(){
-        //将退出的用户从在线列表中删除
-        if(onlineUsers.hasOwnProperty(socket.name)) {
-            //退出用户的信息
-            var obj = {userid:socket.name, username:onlineUsers[socket.name]};
-             
-            //删除
-            delete onlineUsers[socket.name];
-            //在线人数-1
-            onlineCount--;
-             
-            //向所有客户端广播用户退出
-            io.emit('logout', {onlineUsers:onlineUsers, onlineCount:onlineCount, user:obj});
-            console.log(obj.username+'退出了聊天室');
+        if(onlineUsers.hasOwnProperty(socket.id)) {
+            delete onlineUsers[socket.id];
         }
+        pub_space.broadcast.to(socket.id).emit('exit', onlineUsers);
+        console.log('['+onlineUsers[socket.id]+']退出了公共大厅');
     });
-     
-    //监听用户发布聊天内容
+    // 用户消息，自定义事件
     socket.on('message', function(obj){
-        //向所有客户端广播发布的消息
-        io.emit('message', obj);
+        pub_space.to(socket.id).emit('message', obj);
         console.log(obj.username+'说：'+obj.content);
     });
-   
 });
+   
+
